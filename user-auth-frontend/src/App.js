79b05447da9a1ed0518610562dc8c033d4122846
SyedRoshanAli <94375418+SyedRoshanAli@users.noreply.github.com>
2025-01-16 16:55:02 +0500

    Updated code

--- user-auth-frontend/src/App.js
+++ user-auth-frontend/src/App.js
@@ -3,6 +3,7 @@ import Web3 from 'web3';
 import LandingPage from './LandingPage'; // Landing Page Component
 import Register from './Register'; // Register Page Component
 import Login from './Login.js'; // Login Page Component
+import Profile from './Profile/Profile.js'; // Profile Page Component
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -72,6 +73,8 @@ function App() {
                     <Route path="/register" element={<Register account={account} web3={web3} />} />
                     {/* Login Page */}
                     <Route path="/login" element={<Login account={account} />} />
+                    {/* Profile Page */}
+                    <Route path="/profile" element={<Profile account={account} web3={web3} />} />
                 </Routes>
             </div>
         </Router>
